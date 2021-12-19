d991e9a50aca89245cd35bb4d47fa27e4d98f274
Mikaela Hedberg <mikaela.hedberg@plandigital.se>
2021-12-19 10:49:38 +0100

    mui setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
-
+import { Button } from '@mui/material';
+import { purple } from '@mui/material/colors';
 function App() {
-  return <div className='App'>Hello world</div>;
+  console.log(purple);
+  return (
+    <div className='App'>
+      <h1> Hello world</h1>
+      <Button color='primary' variant='contained'>
+        {' '}
+        Hello World
+      </Button>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#474785'
+    },
+    neutral: {
+      main: '#333',
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#47855c'
+    }
+  },
+  typography: {
+    fontFamily: ['Fira Sans', 'sans-serif'].join(',')
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
