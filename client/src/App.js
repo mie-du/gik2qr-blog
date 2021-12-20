0c8a3f1e5af15d6b834933ce24977b850c60d166
Mikaela Hedberg <mie@du.se>
2021-12-20 14:50:55 +0100

    navbara fix, add to 9-6

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import { purple } from '@mui/material/colors';
 
 import Home from './Home';
 import User from './User';
@@ -7,13 +9,29 @@ import Posts from './Posts';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/user' component={User} />
-        <Route exact path='/posts' component={Posts} />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <AppBar component='nav' position='static'>
+          <Toolbar component='ul'>
+            <Typography variant='li' component='p' sx={{ flexGrow: 1 }}>
+              <Link to='/'>Home</Link>
+            </Typography>
+            <Typography variant='body2' component='li'>
+              <Link to='/posts'>Posts</Link>
+            </Typography>
+            <Typography variant='body2' component='li'>
+              <Link to='/user'>Profile</Link>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/user' component={User} />
+          <Route exact path='/posts' component={Posts} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
