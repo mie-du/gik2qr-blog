26461901e5bb7b04314c7b6fbf3730190f2faa4f
Mikaela Hedberg <mie@du.se>
2022-01-10 15:24:06 +0100

    reset to start

--- server/models/post.js
+++ server/models/post.js
@@ -1,24 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define(
-    'post',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      title: {
-        type: DataTypes.STRING(100),
-        allowNull: false
-      },
-      body: {
-        type: DataTypes.TEXT,
-        allowNull: false
-      },
-      imageUrl: {
-        type: DataTypes.STRING(255)
-      }
-    },
-    { underscored: true }
-  );
+  return sequelize.define('post', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    }
+  });
 };
