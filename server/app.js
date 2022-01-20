5ee8f68fa8981adc1b38d86df3ef24dc6ee01ccf
Mikaela Hedberg <mie@du.se>
2022-01-20 13:37:25 +0100

    fix in app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/posts', require('./routes/postsRoute'));
-app.use('/users', require('./routes/tagsRoutee'));
-app.use('/tags', require('./routes/usersRoute'));
+app.use('/users', require('./routes/usersRoute'));
+app.use('/tags', require('./routes/tagsRoute'));
 
 module.exports = app;
