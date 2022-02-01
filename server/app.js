c77fe8c02da5a3a765a0ca53e62d97c8b68b37b8
Mikaela Hedberg <mie@du.se>
2022-02-01 11:58:58 +0100

    removed cors to add later

--- server/app.js
+++ server/app.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
 
 var app = express();
-app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
 
 app.use('/posts', require('./routes/postsRoute'));
 app.use('/users', require('./routes/usersRoute'));
