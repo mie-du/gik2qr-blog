9e7fb2f32abe9fa0465b47a4629982a18ecd87e8
Mikaela Hedberg <mie@du.se>
2022-02-13 16:16:10 +0100

    back-navigations, posts-view

---
+++ client/src/helpers/styles.js
@@ -0,0 +1,19 @@
+import { teal } from '@mui/material/colors';
+
+export const pageTitle = {
+  sx: {
+    textTransform: 'uppercase',
+    marginBottom: '.3em',
+    color: teal[900]
+  },
+  variant: 'h4',
+  component: 'h2'
+};
+export const pageSubtitle = {
+  sx: {
+    textTransform: 'uppercase',
+    color: teal[900]
+  },
+  variant: 'h5',
+  component: 'h3'
+};
