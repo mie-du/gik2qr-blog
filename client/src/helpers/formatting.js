c073149a7448c0a9e0c5d49de3685fd449e50d86
Mikaela Hedberg <mie@du.se>
2022-02-18 12:43:44 +0100

    merge

--- client/src/helpers/formatting.js
+++
@@ -1,8 +0,0 @@
-export const truncate = (str, maxLength) => {
-  return str.length > maxLength ? `${str.substring(0, maxLength)} ...` : str;
-};
-
-export const toDateTimeString = (string) => {
-  const dateObj = new Date(string);
-  return `${dateObj.toLocaleDateString()} ${dateObj.toLocaleTimeString()}`;
-};
